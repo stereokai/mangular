b3fdbcbccd605673fc41bbdce89224129ba7d40f
Stereokai <tom@stereokai.com>
2014-08-04 00:39:35 +0300

    Get rid of Highlight.js (no offence brah)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,6 @@ angular
     'ngSanitize',
     'ngTouch',
     'unicorn-directive',
-    'hljs'
   ])
 
   .config(['$locationProvider', '$httpProvider', function ($locationProvider, $httpProvider) {
